a410d7af4c393faf983c5836209a5a68e6c39349
alicefmc <alicecosta101208@gmail.com>
2022-12-13 15:58:57 -0300

    Projeto 108

--- sign_language.py
+++ sign_language.py
@@ -24,13 +24,34 @@ while True:
                 lm_list.append(lm)
 
              #O código vai aqui  
+            finger_fold_status = []
+            for tip in finger_tips:
+                x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
+                cv2.circle(img, (x,y), 15, (255,0,0), cv2.FILLED)
 
+            if lm_list[tip].x < lm_list[tip - 3].x:
+                cv2.circle(img,(x,y), 15, (0,255,0), cv2.FILLED)
+                finger_fold_status.append(True)
+            else:
+                finger_fold_status.append(False)    
+            
+            if all(finger_fold_status):
+                if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_tip-2].y:
+                    print("CURTI")
+                    cv2.putText(img, "CURTI", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
-
+                if lm_list[thumb_tip].y > lm_list[thumb_tip-1].y > lm_list[thumb_tip-2].y:
+                    print("NAO CURTI")
+                    cv2.putText(img, "NAO CURTI", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
+            
             mp_draw.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
             mp_draw.DrawingSpec((0,255,0),4,2))
     
 
     cv2.imshow("detector de maos", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+    
+cv2.destroyAllWindows()    
